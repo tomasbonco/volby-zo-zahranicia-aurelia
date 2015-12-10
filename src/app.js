47d763a0809976784e15915ff77b3758d701394c
Comper <tomasbonco@gmail.com>
2015-12-11 00:52:25 +0100

    Updated structure added router

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: ['', 'welcome'], name: 'welcome',      	moduleId: 'welcome',      	nav: true, title: 'Welcome' },
+			{ route: 'abroad',				name: 'abroad',      		moduleId: 'abroad',      		nav: true, title: 'Zahraničie' },
+      { route: 'voting-card',   name: 'voting-card',    moduleId: 'voting-card',  	nav: true, title: 'Github Users' },
+      { route: 'voting-letter', name: 'voting-letter', 	moduleId: 'voting-letter', 	nav: true, title: 'Child Router' },
+			{ route: 'pdf-preview', 	name: 'pdf-preview', 		moduleId: 'pdf-preview', 		nav: true, title: 'Child Router' },
+			{ route: 'sign_pdf', 			name: 'sign_pdf', 			moduleId: 'sign_pdf', 			nav: true, title: 'Child Router' },
+			{ route: 'pdf_final', 		name: 'pdf_final', 			moduleId: 'pdf_final', 			nav: true, title: 'Child Router' }
     ]);
 
     this.router = router;
