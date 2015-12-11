0f74dc8eaf5c880182173fbdb576eefd88ef8b10
Comper <tomasbonco@gmail.com>
2015-12-11 01:11:36 +0100

    Added basic structure to files

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ export class App {
     config.title = 'Aurelia';
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      	moduleId: 'welcome',      	nav: true, title: 'Welcome' },
+			{ route: 'intro',					name: 'intro',      		moduleId: 'intro',      		nav: true, title: 'Zahraničie' },
 			{ route: 'abroad',				name: 'abroad',      		moduleId: 'abroad',      		nav: true, title: 'Zahraničie' },
       { route: 'voting-card',   name: 'voting-card',    moduleId: 'voting-card',  	nav: true, title: 'Github Users' },
       { route: 'voting-letter', name: 'voting-letter', 	moduleId: 'voting-letter', 	nav: true, title: 'Child Router' },
 			{ route: 'pdf-preview', 	name: 'pdf-preview', 		moduleId: 'pdf-preview', 		nav: true, title: 'Child Router' },
-			{ route: 'sign_pdf', 			name: 'sign_pdf', 			moduleId: 'sign_pdf', 			nav: true, title: 'Child Router' },
-			{ route: 'pdf_final', 		name: 'pdf_final', 			moduleId: 'pdf_final', 			nav: true, title: 'Child Router' }
+			{ route: 'sign-pdf', 			name: 'sign-pdf', 			moduleId: 'sign-pdf', 			nav: true, title: 'Child Router' },
+			{ route: 'pdf-final', 		name: 'pdf-final', 			moduleId: 'pdf-final', 			nav: true, title: 'Child Router' }
     ]);
 
     this.router = router;
